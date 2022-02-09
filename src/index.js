1854c8ef9232d4e5675c6acf3923783f025cc5eb
Belema <tamunobelemaachego@gmail.com>
2022-02-09 01:21:30 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,25 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { applyMiddleware, createStore } from 'redux';
-import reducer from './store/reducers/reducer';
-import thunk from 'redux-thunk';
-
-
-const store = createStore(
-  reducer, 
-  applyMiddleware(thunk)
-  )
 
 const app =  (
-  <Provider store={store}>
     <BrowserRouter>
       <React.StrictMode>
         <App />
       </React.StrictMode>
     </BrowserRouter>
-  </Provider>
 )
 
 ReactDOM.render(
